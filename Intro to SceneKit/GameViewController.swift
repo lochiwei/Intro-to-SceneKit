2c11b2c8ee331d0fd1aa271cb817b49c82db4814
lochiwei <lochiwei.ios.dev.free@gmail.com>
2018-07-22 20:13:09 +0800

    initial commit

--- Intro to SceneKit/GameViewController.swift
+++ Intro to SceneKit/GameViewController.swift
@@ -1,126 +1,58 @@
-//
-//  GameViewController.swift
-//  Intro to SceneKit
-//
-//  Created by pegasus on 2018/07/22.
-//  Copyright © 2018年 Lo Chiwei. All rights reserved.
-//
+/*
+ Reference:
+ - https://www.weheartswift.com/introduction-scenekit-part-1/
+ 
+ Key Points:
+ - create custom SCNScene subclass so that the scene's logic is self contained.
+ */
 
 import UIKit
 import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
+    
+    // MARK: - Private Properties
+    
+    private var scene: SCNScene!
+    private var sceneView: SCNView!
+    
+    // MARK: - View Controller Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
+        // create and configure a new scene
+        setupSceneView()
+        setupScene()
+    }
+    
+    
+    // MARK: - Scene
+    
+    private func setupScene() {
+        scene = PrimitivesScene()
+        sceneView.scene = scene
+    }
+    
+    private func setupSceneView() {
+
         // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
+        sceneView = view as! SCNView
         
         // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        sceneView.allowsCameraControl = true
+        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     }
     
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
+    // MARK: - View Controller Settings
     
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+    override var shouldAutorotate: Bool { return true }
+    override var prefersStatusBarHidden: Bool { return true }
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+        return UIDevice.current.userInterfaceIdiom == .phone ? .allButUpsideDown : .all
     }
 
-}
+}// end: GameViewController
---
+++ Intro to SceneKit/PrimitivesScene.swift
@@ -0,0 +1,77 @@
+//
+//  PrimitivesScene.swift
+//  Intro to SceneKit
+//
+//  Created by pegasus on 2018/07/22.
+//  Copyright © 2018年 Lo Chiwei. All rights reserved.
+//
+
+import SceneKit
+import Vectors
+
+class PrimitivesScene: SCNScene {
+    
+    // MARK: - init
+    
+    override init() {
+        super.init()
+        setupScene()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupScene()
+    }
+    
+    func setupScene() {
+        spawnNodes()
+    }
+    
+    func spawnNodes(mode:Int = 1) {
+        rootNode.childNodes.forEach { $0.removeFromParentNode() }
+        switch mode {
+        case 1: put3Spheres()
+        case 2: putBallsOnAxis()
+        case 3: putBallsFromSmallToBig(number: 10, radius: 0.5, increment: 0.3)
+        default: put3Spheres()
+        }
+    }
+    
+    func putNode(geometry: SCNGeometry, color:UIColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1), at position:SCNVector3? = nil)  {
+        let node = SCNNode(geometry: geometry)
+        node.geometry?.materials.first?.diffuse.contents = color
+        if let p = position { node.position = p }
+        rootNode.addChildNode(node)
+    }
+    
+    // MARK: - Scene
+    
+    func put3Spheres() {
+        putNode(geometry: SCNSphere(radius: 1))
+        putNode(geometry: SCNSphere(radius: 0.5), color: #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1), at: [3,0,0])
+        putNode(geometry: SCNSphere(radius: 1.5), color: #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), at: [0,3,0])
+    }
+    
+    func putBallsOnAxis(number n: Int = 20, radius r:CGFloat = 1, padding p:CGFloat = 0.2) {
+        for i in 0...(n-1) {
+            let sphere = SCNSphere(radius: r)
+            let color = (i % 2 == 0) ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1) : #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
+            let w = 2*r + p
+            let x = w*i - (n-1)*(w/2)
+            putNode(geometry: sphere, color: color, at: [x, 0, 0])
+        }
+    }
+    
+    func putBallsFromSmallToBig(number n:Int = 10, radius a:CGFloat = 1, increment d:CGFloat = 0.1)  {
+        let len = (n-1)*(2*a + (n-1)*d)
+        for k in 0...(n-1) {
+            let culumated = k*(2*a + k*d)
+            let x = culumated - (len/2)
+            let r = CGFloat(a + k*d)
+            let s = SCNSphere(radius: r)
+            let c = (k % 2 == 0) ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1) : #colorLiteral(red: 0.3236978054, green: 0.1063579395, blue: 0.574860394, alpha: 1)
+            putNode(geometry: s, color: c, at: [x, 0, 0])
+        }
+    }
+    
+}
